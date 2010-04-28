d7ee478aa8de31e4216755d131ff83252d9590fb
ETj <etj@geo-solutions.it>
2010-04-28 17:28:35 +0200

    Fixed path parsers.

--- src/actions/sas/src/main/java/it/geosolutions/geobatch/sas/base/SASDirNameParser.java
+++ src/actions/sas/src/main/java/it/geosolutions/geobatch/sas/base/SASDirNameParser.java
@@ -1,8 +1,11 @@
 /*
  */
-
 package it.geosolutions.geobatch.sas.base;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
@@ -16,28 +19,30 @@ import java.util.regex.Pattern;
  */
 public class SASDirNameParser {
 
-    private String date;
+    private Date date;
     private String mission;
     private String leg;
     private String channel;
-
     private static Pattern PATTERN = Pattern.compile("(.*)_(.*)_Leg(.*)_(.*)");
-//    private static DateFormat DATEFRM = new SimpleDateFormat("yyMMdd");
+    private static DateFormat DATEFRM = new SimpleDateFormat("yyMMdd");
 
     public static SASDirNameParser parse(String fileName) {
-            SASDirNameParser ret = new SASDirNameParser();
-            Matcher m = PATTERN.matcher(fileName);
-            if (!m.find()) {
-                Logger.getLogger(SASDirNameParser.class.getName()).log(Level.SEVERE, "Can't parse '"+fileName+"'");
-                return null;
-            }
+        SASDirNameParser ret = new SASDirNameParser();
+        Matcher m = PATTERN.matcher(fileName);
+        if (!m.find()) {
+            Logger.getLogger(SASDirNameParser.class.getName()).log(Level.SEVERE, "Can't parse '" + fileName + "'");
+            return null;
+        }
+        try {
+            ret.date = (Date) DATEFRM.parse(m.group(1));
+        } catch (ParseException ex) {
+            Logger.getLogger(SASDirNameParser.class.getName()).log(Level.SEVERE, "Can't parse date for '" + fileName + "'", ex);
+        }
+        ret.mission = m.group(2);
+        ret.leg = m.group(3);
+        ret.channel = m.group(4);
 
-            ret.date = m.group(1);
-            ret.mission = m.group(2);
-            ret.leg = m.group(3);
-            ret.channel = m.group(4);
-
-            return ret;
+        return ret;
     }
 
 //	private static String buildWmsPath(final String name) {
@@ -53,14 +58,14 @@ public class SASDirNameParser {
 //        final String wmsPath = new StringBuilder("/").append(timePrefix).append("/").append(missionPrefix).append("/").append(legPath.replace("_","/")).toString();
 //        return wmsPath;
 //	}
-
-    private SASDirNameParser(){}
+    private SASDirNameParser() {
+    }
 
     public String getChannel() {
         return channel;
     }
 
-    public String getDate() {
+    public Date getDate() {
         return date;
     }
 
@@ -71,5 +76,4 @@ public class SASDirNameParser {
     public String getMission() {
         return mission;
     }
-
 }
