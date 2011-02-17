fddd3daf8d9f257112eeb65d2379726455e7cd8d
afabiani <alessio.fabiani@geo-solutions.it>
2011-02-17 16:54:56 +0100

    - fixes for EMSA

--- src/actions/imagemosaic/src/main/java/it/geosolutions/geobatch/imagemosaic/ImageMosaicConfigurator.java
+++ src/actions/imagemosaic/src/main/java/it/geosolutions/geobatch/imagemosaic/ImageMosaicConfigurator.java
@@ -74,6 +74,8 @@ import org.geotools.data.postgis.PostgisNGJNDIDataStoreFactory;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureSource;
 import org.geotools.feature.FeatureIterator;
+import org.geotools.gce.geotiff.GeoTiffReader;
+import org.geotools.geometry.GeneralEnvelope;
 import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.referencing.CRS;
 import org.opengis.feature.simple.SimpleFeature;
@@ -450,18 +452,6 @@ public class ImageMosaicConfigurator extends ImageMosaicConfiguratorAction<FileS
                                                         .getGeometryDescriptor().getLocalName();
                                                 ReferencedEnvelope bounds = featureSource
                                                         .getBounds();
-                                                WKTReader wktReader = new WKTReader();
-                                                Geometry the_geom = wktReader.read("POLYGON(("
-                                                        + bounds.getMinX() + " " + bounds.getMinY()
-                                                        + "," + bounds.getMinX() + " "
-                                                        + bounds.getMaxY() + "," + bounds.getMaxX()
-                                                        + " " + bounds.getMaxY() + ","
-                                                        + bounds.getMaxX() + " " + bounds.getMinY()
-                                                        + "," + bounds.getMinX() + " "
-                                                        + bounds.getMinY() + "))");
-                                                Integer SRID = CRS.lookupEpsgCode(bounds
-                                                        .getCoordinateReferenceSystem(), true);
-                                                the_geom.setSRID(SRID);
 
                                                 Properties ftProps = Utils
                                                         .loadPropertiesFromURL(DataUtilities
@@ -504,9 +494,26 @@ public class ImageMosaicConfigurator extends ImageMosaicConfiguratorAction<FileS
                                                     // create a new feature
                                                     final SimpleFeature feature = fw.next();
 
-                                                    // get attributes and copy them
-                                                    // over
+                                                    // get attributes and copy them over
 
+                                                    GeoTiffReader reader = new GeoTiffReader(new File(inputDir, granuleLocation));
+                                                    GeneralEnvelope originalEnvelope = reader.getOriginalEnvelope();
+                                                    
+                                                    ReferencedEnvelope bb = new ReferencedEnvelope(originalEnvelope);
+                                                    
+                                                    WKTReader wktReader = new WKTReader();
+                                                    Geometry the_geom = wktReader.read("POLYGON(("
+                                                            + bb.getMinX() + " " + bb.getMinY()
+                                                            + "," + bb.getMinX() + " "
+                                                            + bb.getMaxY() + "," + bb.getMaxX()
+                                                            + " " + bb.getMaxY() + ","
+                                                            + bb.getMaxX() + " " + bb.getMinY()
+                                                            + "," + bb.getMinX() + " "
+                                                            + bb.getMinY() + "))");
+                                                    Integer SRID = CRS.lookupEpsgCode(bb
+                                                            .getCoordinateReferenceSystem(), true);
+                                                    the_geom.setSRID(SRID);
+                                                    
                                                     feature.setAttribute(geometryPropertyName,
                                                             the_geom);
                                                     feature.setAttribute("location",
@@ -830,7 +837,7 @@ public class ImageMosaicConfigurator extends ImageMosaicConfiguratorAction<FileS
                 String coverageStoreId = inputDir.getName();
 
                 LOGGER.info("Coverage Store ID: " + coverageStoreId);
-
+                
                 mosaicDescriptor = new ImageMosaicGranulesDescriptor(coverageStoreId, metocFields,
                         firstCvNameParts, lastCvNameParts);
             }
