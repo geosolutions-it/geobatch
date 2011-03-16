3f8ad85ed39df55c52ffc20843f004cf19ce5f64
ccancellieri <carlo.cancellieri@geo-solutions.it>
2011-03-16 19:34:55 +0100

    EMSA fixes and tools improvements.

--- src/actions/imagemosaic/src/main/java/it/geosolutions/geobatch/imagemosaic/ImageMosaicGranulesDescriptor.java
+++ src/actions/imagemosaic/src/main/java/it/geosolutions/geobatch/imagemosaic/ImageMosaicGranulesDescriptor.java
@@ -3,15 +3,12 @@ package it.geosolutions.geobatch.imagemosaic;
 import it.geosolutions.geobatch.tools.file.Collector;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.apache.commons.cli2.util.Comparators;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOCase;
 import org.apache.commons.io.filefilter.SuffixFileFilter;
@@ -20,14 +17,17 @@ import org.apache.commons.io.filefilter.SuffixFileFilter;
  * 
  * @author afabiani
  * 
- * NOTE: may we want to use some other already defined classes ???
- * GranuleDescriptor....
+ *         NOTE: may we want to use some other already defined classes ??? GranuleDescriptor....
  * 
  */
 final class ImageMosaicGranulesDescriptor {
     private final static Logger LOGGER = Logger.getLogger(ImageMosaicGranulesDescriptor.class
             .toString());
-    
+
+    // TODO a better file filter
+    private final static Collector coll = new Collector(new SuffixFileFilter(new String[] { ".tif",
+            ".tiff" }, IOCase.INSENSITIVE));
+
     private String coverageStoreId = null;
 
     private String metocFields = null;
@@ -77,7 +77,28 @@ final class ImageMosaicGranulesDescriptor {
     public String[] getLastCvNameParts() {
         return lastCvNameParts;
     }
-    
+
+    protected static ImageMosaicGranulesDescriptor buildDescriptor(ImageMosaicCommand cmd) {
+        File inputDir = cmd.getBaseDir();
+
+        List<File> fileNameList = coll.collect(inputDir);
+        if (cmd.getAddFiles() != null) {
+            fileNameList.addAll(cmd.getAddFiles());
+        }
+        if (cmd.getDelFiles() != null) {
+            fileNameList.removeAll(cmd.getDelFiles());
+        }
+
+        if (fileNameList == null) {
+            if (LOGGER.isLoggable(Level.INFO)) {
+                LOGGER.info("ImageMosaicGranulesDescriptor:buildDescriptor(): unable to collect files from the dir: "
+                        + inputDir.getAbsolutePath());
+            }
+            return null;
+        }
+
+        return buildDescriptor(inputDir, fileNameList);
+    }
 
     /**
      * Try to parse the input File name to build an Image Mosaic Descriptor
@@ -87,41 +108,54 @@ final class ImageMosaicGranulesDescriptor {
      * @return
      */
     protected static ImageMosaicGranulesDescriptor buildDescriptor(File inputDir) {
-        ImageMosaicGranulesDescriptor mosaicDescriptor = null;
-//TODO a better file filter
-        Collector coll=new Collector(new SuffixFileFilter(new String[]{ ".tif", ".tiff" },IOCase.INSENSITIVE));
-        
-        List<File> fileNameList=null;
-        try {
-            fileNameList = coll.collect(inputDir);
-        } catch (IOException e) {
-            if (LOGGER.isLoggable(Level.SEVERE))
-                LOGGER.severe("ImageMosaicGranulesDescriptor:buildDescriptor(): unable to collect files from the dir: "
-                        +inputDir.getAbsolutePath()+". Message:"+e.getLocalizedMessage());
-            return null;
-        }
-        
-        if (fileNameList==null){
-            if (LOGGER.isLoggable(Level.INFO)){
-                LOGGER.info(
-                        "ImageMosaicGranulesDescriptor:buildDescriptor(): unable to collect files from the dir: "
-                        +inputDir.getAbsolutePath());
+
+        List<File> fileNameList = null;
+
+        fileNameList = coll.collect(inputDir);
+
+
+        if (fileNameList == null) {
+            if (LOGGER.isLoggable(Level.INFO)) {
+                LOGGER.info("ImageMosaicGranulesDescriptor:buildDescriptor(): unable to collect files from the dir: "
+                        + inputDir.getAbsolutePath());
             }
             return null;
         }
-        
+
+        return buildDescriptor(inputDir, fileNameList);
+    }
+
+    /**
+     * 
+     * @param fileNameList
+     *            (must be not null)
+     * @param inputDir
+     *            baseDir (must be not null)
+     * @return
+     */
+    private static ImageMosaicGranulesDescriptor buildDescriptor(File inputDir,
+            List<File> fileNameList) {
+
+        ImageMosaicGranulesDescriptor mosaicDescriptor = null;
+
         // to get it ordered from the first to the last file (by Name)
-        Collections.sort(fileNameList);
-        
-        //fileNames = fileNameList.toArray(new String[1]);
+        Collections.sort(fileNameList, new Comparator<File>() {
+            public int compare(File o1, File o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+
+        // fileNames = fileNameList.toArray(new String[1]);
 
         // Store ID
         String coverageStoreId = inputDir.getName();
         //
         if (fileNameList.size() > 0) {
-            
-            String[] firstCvNameParts = FilenameUtils.getBaseName(fileNameList.get(0).getName()).split("_");
-            String[] lastCvNameParts = FilenameUtils.getBaseName(fileNameList.get(fileNameList.size()-1).getName()).split("_");
+
+            String[] firstCvNameParts = FilenameUtils.getBaseName(fileNameList.get(0).getName())
+                    .split("_");
+            String[] lastCvNameParts = FilenameUtils.getBaseName(
+                    fileNameList.get(fileNameList.size() - 1).getName()).split("_");
 
             if (firstCvNameParts != null && firstCvNameParts.length > 3) {
                 // TODO:
@@ -139,31 +173,26 @@ final class ImageMosaicGranulesDescriptor {
                         .append(firstCvNameParts[7]).append("_") // TAU
                         .append(firstCvNameParts[8]) // NoDATA
                         .toString() : inputDir.getName();
-                
-                if (LOGGER.isLoggable(Level.INFO)){
-                    LOGGER.info("ImageMosaicGranulesDescriptor:buildDescriptor(): Coverage Store ID: " + coverageStoreId);
+
+                if (LOGGER.isLoggable(Level.INFO)) {
+                    LOGGER.info("ImageMosaicGranulesDescriptor:buildDescriptor(): Coverage Store ID: "
+                            + coverageStoreId);
                 }
 
                 mosaicDescriptor = new ImageMosaicGranulesDescriptor(coverageStoreId, metocFields,
                         firstCvNameParts, lastCvNameParts);
-            }
-            else {
+            } else {
                 mosaicDescriptor = new ImageMosaicGranulesDescriptor(coverageStoreId,
-                                                                    coverageStoreId,
-                                                                    firstCvNameParts,
-                                                                    lastCvNameParts);
+                        coverageStoreId, firstCvNameParts, lastCvNameParts);
             }
-        }
-        else {
-            if (LOGGER.isLoggable(Level.WARNING)){
-                LOGGER.warning(
-                    "ImageMosaicGranulesDescriptor:buildDescriptor(): The passed base dir is empty! Dir:"+inputDir.getAbsolutePath());
+        } else {
+            if (LOGGER.isLoggable(Level.WARNING)) {
+                LOGGER.warning("ImageMosaicGranulesDescriptor:buildDescriptor(): The passed base dir is empty! Dir:"
+                        + inputDir.getAbsolutePath());
             }
-            
-            mosaicDescriptor = new ImageMosaicGranulesDescriptor(coverageStoreId,
-                    coverageStoreId,
-                    null,
-                    null);
+
+            mosaicDescriptor = new ImageMosaicGranulesDescriptor(coverageStoreId, coverageStoreId,
+                    null, null);
         }
         return mosaicDescriptor;
     }
--- src/gb-tools/src/main/java/it/geosolutions/geobatch/tools/file/Collector.java
+++ src/gb-tools/src/main/java/it/geosolutions/geobatch/tools/file/Collector.java
@@ -28,88 +28,111 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.apache.commons.io.DirectoryWalker;
-import org.apache.commons.io.IOCase;
 import org.apache.commons.io.filefilter.FileFilterUtils;
-import org.apache.commons.io.filefilter.WildcardFileFilter;
 
 /**
- * A collector which wraps the DirectoryWalker to define a Collector
- * which is able to collect (recursively) a set of file starting from
- * a base dir applying a FileFilter 
+ * A collector which wraps the DirectoryWalker to define a Collector which is able to collect
+ * (recursively) a set of file starting from a base dir applying a FileFilter
  * 
  * @author Carlo Cancellieri - carlo.cancellieri@geo-solutions.it
- *
+ * 
  */
-public class Collector extends DirectoryWalker<File>{
-    private FileFilter filter=null;
+public class Collector extends DirectoryWalker<File> {
+    private final static Logger LOGGER=Logger.getLogger(Collector.class.toString());
     
-    public Collector(FileFilter filter){
-        super(null,-1);
-        this.filter=filter;
+    private FileFilter filter = null;
+
+    public Collector(FileFilter filter) {
+        super(null, -1);
+        this.filter = filter;
     }
-    
+
     /**
-     * @param filter the filter to apply, null means visit all files
-     * @param deep controls how deep the hierarchy is navigated to 
-     * (less than 0 means unlimited)
+     * @param filter
+     *            the filter to apply, null means visit all files
+     * @param deep
+     *            controls how deep the hierarchy is navigated to (less than 0 means unlimited)
      */
-    public Collector(FileFilter filter, int deep){
-        super(null,deep);
-        this.filter=filter;
+    public Collector(FileFilter filter, int deep) {
+        super(null, deep);
+        this.filter = filter;
     }
-    
-    public List<File> collect(File root) throws IOException{
-        List<File> res=new ArrayList<File>();
-        super.walk(root, res);
-        return res;
+
+    /**
+     * Set a new filter for this colletctor. (can be null)
+     * @param filter
+     */
+    public void setFilter(FileFilter filter) {
+        this.filter = filter;
     }
-    
+
+    /**
+     * 
+     * @param root
+     * @return list of file (can be empty)
+     * @throws IOException
+     */
+    public List<File> collect(File root) {
+        if (root!=null){
+            List<File> res = new ArrayList<File>();
+            try{
+                super.walk(root, res);
+            } catch (NullPointerException npe){
+                // if the start directory is null
+                if (LOGGER.isLoggable(Level.WARNING)){
+                    LOGGER.log(Level.WARNING,"Collector::collect() the start directory is null",npe);
+                }
+                return null;
+            } catch (IOException ioe){
+                // if an I/O Error occurs
+                if (LOGGER.isLoggable(Level.WARNING)){
+                    LOGGER.log(Level.WARNING,"Collector::collect() an I/O Error occurs.",ioe);
+                }
+                return null;
+            }
+            return res;
+        }
+        else {
+            // if the start directory is null
+            if (LOGGER.isLoggable(Level.WARNING)){
+                LOGGER.log(Level.WARNING, "Collector::collect() the start directory is null");
+            }
+            return null;
+        }
+            
+    }
+
     @Override
-    protected boolean handleDirectory(File directory, int depth, Collection<File> results) throws IOException {
-        if (this.filter!=null){
-            if (this.filter.accept(directory)){
+    protected boolean handleDirectory(File directory, int depth, Collection<File> results)
+            throws IOException {
+        if (this.filter != null) {
+            if (this.filter.accept(directory)) {
                 results.add(directory);
             }
         }
-        return true;  // process directory
+        return true; // process directory
     }
-    
+
     @Override
     protected File[] filterDirectoryContents(File directory, int depth, File[] files)
             throws IOException {
-//        return directory.listFiles(this.filter);
-        if (this.filter!=null)
-            return directory.listFiles((FilenameFilter)FileFilterUtils.or(FileFilterUtils.directoryFileFilter(),FileFilterUtils.asFileFilter(this.filter)));
+        if (this.filter != null)
+            return directory.listFiles((FilenameFilter) FileFilterUtils.or(
+                    FileFilterUtils.directoryFileFilter(),
+                    FileFilterUtils.asFileFilter(this.filter)));
         else
             return directory.listFiles();
     }
 
     @Override
     protected void handleFile(File file, int depth, Collection<File> results) throws IOException {
-        if (this.filter!=null)
-            if (this.filter.accept(file));
+        if (this.filter != null)
+            if (this.filter.accept(file))
                 results.add(file);
-    }
-
-    /**
-     * TODO test 
-     * @deprecated a unit test should be implemented
-     */
-    public static void main(String []arg0){
-        Collector c=new Collector(//new WildcardFileFilter("*",IOCase.INSENSITIVE));
-                FileFilterUtils.or(
-                        new WildcardFileFilter("*_PCK.xml",IOCase.INSENSITIVE),
-                        new WildcardFileFilter("*_PRO",IOCase.INSENSITIVE)));
-        try {
-            List<File> list=c.collect(new File("/home/carlo/work/data/EMSAWorkingDir/out/20110118T084207016UTC"));
-            System.out.println("Number of files: "+list.size());
-            for (File f : list)
-                System.out.println("FILE: "+f.getAbsolutePath());
-        } catch (IOException e) {
-            System.err.println("ERROR: "+e.getLocalizedMessage());
-        }
     }
 
 }
