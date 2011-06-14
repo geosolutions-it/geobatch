d29115a3402bd7177b807bc91f92bc101f784bde
ccancellieri <carlo.cancellieri@geo-solutions.it>
2011-06-14 15:55:48 +0200

    added build_archetype.sh which assolve Task #96

---
+++ .build/build.sh
@@ -0,0 +1,1 @@
+mvn clean install eclipse:clean eclipse:eclipse  -DdownloadSources=true  -DdownloadJavadocs=true  -Declipse.addVersionToProjectName=true -Pdao.xstream -Dall -Dmaven.test.skip=true -e -o
