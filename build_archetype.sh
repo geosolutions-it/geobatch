7fd4d3cb3c0d174b4fd262448d3f73e5ec151261
ccancellieri <carlo.cancellieri@geo-solutions.it>
2011-06-29 11:18:10 +0200

    license to the build_archetype.sh script

--- build_archetype.sh
+++ build_archetype.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+#  * $Header: build_archetype.sh $
+#  * $Revision: 0.1 $
+#  * $Date: 14/giu/2011 18.16.47 $
+#  *
+#  * ====================================================================
+#  *
+#  * Copyright (C) 2007-2011 GeoSolutions S.A.S.
+#  *
+#  * This program is free software; you can redistribute it and/or modify
+#  * it under the terms of the GNU General Public License as published by
+#  * the Free Software Foundation; either version 2 of the License, or
+#  * (at your option) any later version.
+#  *
+#  * This program is distributed in the hope that it will be useful,
+#  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  * GNU General Public License for more details.
+#  *
+#  * You should have received a copy of the GNU General Public License
+#  * along with this program. 
+#  *
+#  * ====================================================================
+#  *
+#  * This software consists of voluntary contributions made by developers
+#  * of GeoSolutions.  For more information on GeoSolutions, please see
+#  * <http://www.geo-solutions.it/>.
 ###################################################
 # usage
 function usage {
