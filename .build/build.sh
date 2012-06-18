48bceb8279bc83ab233c24b1aa3045257a7de4cf
Notarnicola <emanuele.notarnicola@geo-solutions.it>
2012-06-18 18:52:10 +0200

    FAO gif

--- .build/build.sh
+++
@@ -1,1 +0,0 @@
-mvn clean install eclipse:clean eclipse:eclipse  -DdownloadSources=true  -DdownloadJavadocs=true  -Declipse.addVersionToProjectName=true -Pdao.xstream -Dall -Dmaven.test.skip=true -e -o
--- src/build.bat
+++ src/build.bat
@@ -1,1 +1,1 @@
-mvn clean install eclipse:clean eclipse:eclipse -DdownloadSources=true  -DdownloadJavadocs=true -Declipse.addVersionToProjectName=true -Dall -P-geostore -Dmaven.test.skip=true -e
+mvn clean install eclipse:clean eclipse:eclipse -DdownloadSources=true  -DdownloadJavadocs=true -Declipse.addVersionToProjectName=true -Dall -P-geostore -Pdao.xstream -Dmaven.test.skip=true -e -o
