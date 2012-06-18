f786bf06ca875c53b12b330c3c84511b4403c2fb
Damiano <damianofds@gmail.com>
2012-06-18 18:54:35 +0200

    build.bat modification

--- src/build.bat
+++ src/build.bat
@@ -1,1 +1,9 @@
-mvn clean install eclipse:clean eclipse:eclipse -DdownloadSources=true  -DdownloadJavadocs=true -Declipse.addVersionToProjectName=true -Dall -P-geostore -Dmaven.test.skip=true -e
+::supported actions
+set PROFILES=geotiff,shapefile,task-executor,freemarker,scripting,commons
+
+::unsupported
+::PROFILES="$PROFILES,xstream,shp2pg,geonetwork,geostore,octave,jmx,jms"
+::configure the command
+set CMD=mvn clean install eclipse:clean eclipse:eclipse -P%PROFILES% -e %1%
+
+%CMD%
