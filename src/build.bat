e3910613c4e1acbda0e68aa6369b05e3aa559568
Damiano <damianofds@gmail.com>
2012-06-19 12:34:01 +0200

    fixes problem with win xp

--- src/build.bat
+++ src/build.bat
@@ -1,10 +1,11 @@
-
-::supported actions
+@echo off
+rem supported actions
 set PROFILES=geotiff,shapefile,task-executor,freemarker,scripting,commons
 
-::unsupported
-::PROFILES="$PROFILES,xstream,shp2pg,geonetwork,geostore,octave,jmx,jms"
-::configure the command
+rem unsupported
+rem PROFILES="$PROFILES,xstream,shp2pg,geonetwork,geostore,octave,jmx,jms"
+rem configure the command
+
 set CMD=mvn clean install eclipse:clean eclipse:eclipse -P%PROFILES% -e %1%
 
 %CMD%
