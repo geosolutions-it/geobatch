5dfd56141c97bbb787ec1fbe6e0964da58f46482
ccancellieri <carlo.cancellieri@geo-solutions.it>
2012-06-25 10:25:19 +0200

    continue cleanup of pom #31

--- src/doc/make.bat
+++ src/doc/make.bat
@@ -3,7 +3,9 @@
 REM Command file for Sphinx documentation
 
 set SPHINXBUILD=sphinx-build
-set BUILDDIR=build
+if NOT "%BUILDDIR%" == "" (
+	set BUILDDIR=build
+)
 set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% source
 if NOT "%PAPER%" == "" (
 	set ALLSPHINXOPTS=-D latex_paper_size=%PAPER% %ALLSPHINXOPTS%
