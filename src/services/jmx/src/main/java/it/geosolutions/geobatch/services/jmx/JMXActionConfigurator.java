06d60f15f37ae55b0a83286c8d3b65565fef6ea0
ccancellieri <ccancellieri@hotmail.com>
2012-09-28 18:32:49 +0200

    JMX improvements to configure flows on the fly. Added some usefull (to the client) classes.

---
+++ src/services/jmx/src/main/java/it/geosolutions/geobatch/services/jmx/JMXActionConfigurator.java
@@ -0,0 +1,180 @@
+package it.geosolutions.geobatch.services.jmx;
+
+import it.geosolutions.geobatch.configuration.event.action.ActionConfiguration;
+import it.geosolutions.geobatch.flow.event.action.ActionService;
+
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.PropertyUtils;
+import org.slf4j.Logger;
+import org.springframework.context.ApplicationContext;
+
+public class JMXActionConfigurator {
+
+    public static ActionConfiguration configureAction(Logger LOGGER, final Map<String, String> config,
+            ApplicationContext context) throws SecurityException, NoSuchMethodException,
+            IllegalArgumentException, InstantiationException, IllegalAccessException,
+            InvocationTargetException {
+
+        final String serviceId = config.remove(ConsumerManager.SERVICE_ID_KEY);
+        if (serviceId == null || serviceId.isEmpty())
+            throw new IllegalArgumentException("Unable to locate the key "
+                    + ConsumerManager.SERVICE_ID_KEY
+                    + " matching the serviceId action in the passed paramether table");
+
+        final ActionService service = (ActionService) context.getBean(serviceId);
+        final Class serviceClass = service.getClass();
+
+        ActionConfiguration actionConfig = null;
+        for (Method method : serviceClass.getMethods()) {
+            if (method.getName().equals("canCreateAction")) {
+                final Class[] classes = method.getParameterTypes();
+
+                Constructor constructor;
+
+                // BeanUtils.instantiate(clazz)
+                try {
+                    constructor = classes[0].getConstructor(new Class[] {});
+                    actionConfig = (ActionConfiguration) constructor.newInstance();
+                } catch (NoSuchMethodException e) {
+                    constructor = classes[0].getConstructor(new Class[] { String.class,
+                            String.class, String.class });
+                    actionConfig = (ActionConfiguration) constructor.newInstance(serviceId,
+                            serviceId, serviceId);
+                }
+                actionConfig.setServiceID(serviceId);
+                final Set<String> keys = config.keySet();
+                final Iterator<String> it = keys.iterator();
+                while (it.hasNext()) {
+                    String key = it.next();
+                    try {
+                        // TODO add a pluggable configurators from the context
+                        smartCopy(LOGGER, actionConfig, key, config.get(key));
+                    } catch (Exception e) {
+                        if (LOGGER.isErrorEnabled())
+                            LOGGER.error(e.getLocalizedMessage(), e);
+                        // TODO something else?
+                    }
+                }
+
+                if (actionConfig != null)
+                    break;
+            }
+        }
+
+        return actionConfig;
+    }
+
+    private static <T> void smartCopy(final Logger LOGGER, final T bean, final String propertyName,
+            final Object value) throws Exception {
+        // special cases
+        PropertyDescriptor pd = PropertyUtils.getPropertyDescriptor(bean, propertyName);
+        // return null if there is no such descriptor
+        if (pd == null) {
+            return;
+        }
+
+        Class type = pd.getPropertyType();
+
+        Object valueTo = value;
+        if (type.isAssignableFrom(value.getClass())) {
+            // try using setter
+            if (pd.getWriteMethod() != null) {
+                PropertyUtils.setProperty(bean, propertyName, valueTo);
+                return;
+            } else {
+                // T interface doesn't declare setter method for this property
+                // lets use getter methods to get the property reference
+                Object property = PropertyUtils.getProperty(bean, propertyName);
+                if (property == null) {
+                    if (LOGGER.isErrorEnabled())
+                        LOGGER.error("Skipping unwritable property " + propertyName);
+                } else {
+                    if (Collection.class.isAssignableFrom(type)) {
+                        ((Collection) property).addAll((Collection) value);
+                    } else if (Map.class.isAssignableFrom(type)) {
+                        ((Map) property).putAll((Map) value);
+                    }
+                }
+            }
+        } else if (Collection.class.isAssignableFrom(type)) {
+            valueTo = adaptCollection(value);
+
+        } else if (Map.class.isAssignableFrom(type)) {
+
+            valueTo = adaptMap(value);
+
+        } else {
+
+            // fail
+            // try quick way
+            try {
+                BeanUtils.copyProperty(bean, propertyName, valueTo);
+                return;
+            } catch (Exception e) {
+                if (LOGGER.isWarnEnabled())
+                    LOGGER.warn("Error using ");
+            }
+        }
+
+        //
+        // boolean status = true;
+        // if (!status) {
+        // if (LOGGER.isErrorEnabled())
+        // LOGGER.error("Skipping unwritable property " + propertyName
+        // + " unable to find the adapter for type " + pd.getPropertyType());
+        // }
+    }
+
+    private static Map adaptMap(Object value) {
+        Map<Object, Object> liveMap = new HashMap<Object, Object>();
+        // value should be a list of key=value string ';' separated
+        String[] listString = ((String) value).split(";");
+        for (String kvString : listString) {
+            String kv[] = kvString.split("=");
+            liveMap.put(kv[0], kv[1]);
+        }
+        return liveMap;
+    }
+
+    private static Collection adaptCollection(Object value) {
+
+        Collection<Object> liveCollection = new ArrayList<Object>();
+        // value should be a list of string ',' separated
+        String[] listString = ((String) value).split(",");
+        for (String s : listString) {
+            liveCollection.add(s);
+        }
+        return liveCollection;
+    }
+
+    /*
+     * TODO make smart adapter extensible
+     * 
+     * public interface TypeAdapter<F, T> { public boolean adapt(Class<F> fromType, Class<T> toType, Object property, Object value); };
+     * 
+     * private final class StringToCollectionAdapter implements TypeAdapter<String, Map> {
+     * 
+     * @Override public boolean adapt(Class<String> fromType, Class<Map> toType, Object property, Object value) {
+     * 
+     * // check type of property to apply new value if (Map.class.isAssignableFrom(toType)) {
+     * 
+     * final Map<Object, Object> liveMap; if (property != null) { liveMap = (Map<Object, Object>)property; liveMap.clear(); } else { return false; }
+     * if (fromType.isAssignableFrom(value.getClass())) { // value should be a list of key=value string ';' separated String[] listString =
+     * ((String)value).split(";"); for (String kvString : listString) { String kv[] = kvString.split("="); liveMap.put(kv[0], kv[1]); } return true; }
+     * }
+     * 
+     * return false; } }
+     */
+
+}
--- src/services/jmx/src/main/java/it/geosolutions/geobatch/services/jmx/JMXClientUtils.java
+++ src/services/jmx/src/main/java/it/geosolutions/geobatch/services/jmx/JMXClientUtils.java
@@ -45,174 +45,201 @@ import javax.management.remote.JMXServiceURL;
  */
 public class JMXClientUtils {
 
-	// JMX bean
-	public final static String GB_JMXBEAN_KEY = "JMXServiceManager";
-	public final static String GB_JMXBEAN_NAME = "JMXServiceManager";
+    // JMX bean
+    public final static String GB_JMXBEAN_KEY = "JMXServiceManager";
 
-	// JMX
-	public final static String GB_URL_KEY = "gb_jmx_url";
-	public final static String GB_URL = "localhost";
-	public final static String GB_PORT_KEY = "gb_jmx_port";
-	public final static String GB_PORT = "1099";
+    public final static String GB_JMXBEAN_NAME = "JMXServiceManager";
 
-	// delay in secs
-	public static final String PROCESS_DELAY_KEY = "PROCESS_DELAY";
-	public static final Long PROCESS_DELAY_DEFAULT = new Long(1000);// delay in
-																	// secs == 1
+    /**
+     * key to specify the path where to find the properties file to get the connection parameters
+     */
+    public final static String CONNECTION_PARAMETERS_KEY = "CONNECTION_PARAMETERS";
 
-	// sec
+    // JMX
+    /**
+     * connections parameters
+     */
+    public final static String GB_URL_KEY = "gb_jmx_url";
 
-	/**
-	 * initialize environment using the incoming properties file (or defaults).
-	 * The properties file used to configure connection parameters may contains
-	 * at least: <br>
-	 * <ul>
-	 * <li><b>gb_jmx_url=localhost</b> - remote JMX server url</li>
-	 * <li><b>gb_jmx_port=1099</b> - remote JMX server port</li>
-	 * <li><b>JMXActionManager=JMXActionManager</b> - bean name which implements
-	 * ActionManager interface</li>
-	 * </ul>
-	 * NOTE: above keywords are reserved keys<br>
-	 * 
-	 * @param configFilePath
-	 *            the String representing the absolute file path of the
-	 *            jmx.properties or null (in this case defaults values are used)
-	 * @return a map containing the initialized environment
-	 */
-	public static Map<String, String> loadEnv(final String configFilePath) {
-		// load from file
-		final Properties props = new Properties();
-		// if no external file configuration is found using defaults
-		if (configFilePath != null) {
-			FileInputStream fis = null;
-			try {
-				fis = new FileInputStream(new File(configFilePath));
-				props.load(fis);
+    public final static String GB_URL = "localhost";
 
-			} catch (IOException e) {
+    public final static String GB_PORT_KEY = "gb_jmx_port";
 
-				// if (LOGGER.isEnabledFor(Level.ERROR)){
-				// LOGGER.error("Unable to run without a property file, check the path: "+path);
-				// }
+    public final static String GB_PORT = "1099";
 
-			} finally {
-				if (fis != null) {
-					try {
-						fis.close();
-					} catch (Exception e) {
-					}
-				}
-			}
-		}
+    
+    // delay in secs
+    public static final String PROCESS_DELAY_KEY = "PROCESS_DELAY";
 
-		// return env
-		final Map<String, String> env = new HashMap<String, String>();
+    public static final Long PROCESS_DELAY_DEFAULT = new Long(1000);// delay in
+                                                                    // secs == 1
+    
+    /**
+     * Process delay secs
+     * 
+     * @param config
+     * @return
+     */
+    public static long parsePollingDelay(Map<String, String> config) {
 
-		// init defaults
-		// url
-		if (!props.containsKey(GB_URL_KEY))
-			env.put(GB_URL_KEY, GB_URL);
-		else
-			env.put(GB_URL_KEY, props.getProperty(GB_URL_KEY));
+        final String delay = config.get(PROCESS_DELAY_KEY);
+        try {
+            if (delay != null)
+                return Math.round(1000 * Double.parseDouble(delay));
+        } catch (NumberFormatException nfe) {
 
-		// port
-		if (!props.containsKey(GB_PORT_KEY))
-			env.put(GB_PORT_KEY, GB_PORT);
-		else
-			env.put(GB_PORT_KEY, props.getProperty(GB_PORT_KEY));
+        }
+        return PROCESS_DELAY_DEFAULT;
+    }
 
-		if (!props.containsKey(GB_JMXBEAN_KEY))
-			env.put(GB_JMXBEAN_KEY, GB_JMXBEAN_NAME);
-		else
-			env.put(GB_JMXBEAN_KEY, props.getProperty(GB_JMXBEAN_KEY));
+    /**
+     * Process delay secs
+     * 
+     * @param config
+     * @return
+     */
+    public static long parsePollingDelay(Properties config) {
 
-		// form properties env to map env
-		final Set keySet = props.keySet();
-		final Iterator it = keySet.iterator();
-		while (it.hasNext()) {
-			String key = (String) it.next();
-			env.put(key, props.getProperty(key));
-		}
-		return env;
-	}
+        final String delay = config.getProperty(PROCESS_DELAY_KEY);
+        try {
+            if (delay != null)
+                return Math.round(1000 * Double.parseDouble(delay));
+        } catch (NumberFormatException nfe) {
 
-	/**
-	 * 
-	 * @param env
-	 * @return JMXConnector representing the newly-made connection. Each
-	 *         successful call to this method produces a different object
-	 * @throws IllegalArgumentException
-	 *             if the passed env is null, or it doesn't contains valid
-	 *             values for the {@link #GB_PORT_KEY} of {@link #GB_URL_KEY}
-	 * @throws IOException
-	 *             if the connector client or the connection cannot be made
-	 *             because of a communication problem.
-	 */
-	public static JMXConnector getConnector(Map<String, String> env)
-			throws IllegalArgumentException, IOException {
-		if (env == null)
-			throw new IllegalArgumentException(
-					"Unable to connect with a null environment, please");
+        }
+        return PROCESS_DELAY_DEFAULT;
+    }
+    // sec
 
-		final String gbUrl = env.get(GB_URL_KEY);
-		if (gbUrl == null)
-			throw new IllegalArgumentException(
-					"Unable to connect with a null port, please set a port value for the key: "
-							+ GB_URL_KEY);
+    /**
+     * initialize environment using the incoming properties file (or defaults). The properties file used to configure connection parameters may
+     * contains at least: <br>
+     * <ul>
+     * <li><b>gb_jmx_url=localhost</b> - remote JMX server url</li>
+     * <li><b>gb_jmx_port=1099</b> - remote JMX server port</li>
+     * <li><b>JMXActionManager=JMXActionManager</b> - bean name which implements ActionManager interface</li>
+     * </ul>
+     * NOTE: above keywords are reserved keys<br>
+     * 
+     * @param configFilePath the String representing the absolute file path of the jmx.properties or null (in this case defaults values are used)
+     * @return a map containing the initialized environment
+     */
+    public static Map<String, String> loadEnv(final String configFilePath) {
+        // load from file
+        final Properties props = new Properties();
+        // if no external file configuration is found using defaults
+        if (configFilePath != null) {
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(new File(configFilePath));
+                props.load(fis);
 
-		final String gbPort = env.get(GB_PORT_KEY);
-		if (gbPort == null)
-			throw new IllegalArgumentException(
-					"Unable to connect with a null port, please set a url value for the key: "
-							+ GB_PORT_KEY);
+            } catch (IOException e) {
 
-		StringBuilder urlString = new StringBuilder(
-				"service:jmx:rmi:///jndi/rmi://");
+                // if (LOGGER.isEnabledFor(Level.ERROR)){
+                // LOGGER.error("Unable to run without a property file, check the path: "+path);
+                // }
 
-		final JMXServiceURL url = new JMXServiceURL(urlString.append(gbUrl)
-				.append(":").append(gbPort).append("/jmxrmi").toString());
+            } finally {
+                if (fis != null) {
+                    try {
+                        fis.close();
+                    } catch (Exception e) {
+                    }
+                }
+            }
+        }
 
-		return JMXConnectorFactory.connect(url, env);
-	}
+        // return env
+        final Map<String, String> env = new HashMap<String, String>();
 
-	/**
-	 * Using the initialized environment (see {@link #loadEnv(String)}) to
-	 * create a dedicated proxy of the desired MBean (instead of going directly
-	 * through the MBean server connection).
-	 * 
-	 * @param environment
-	 *            see {@link #loadEnv(String)}
-	 * @return the ActionManager proxy instance.
-	 * @throws IOException
-	 *             if a valid MBeanServerConnection cannot be created, for
-	 *             instance because the connection to the remote MBean server
-	 *             has not yet been established (with the connect method), or it
-	 *             has been closed, or it has broken.
-	 * @throws MalformedObjectNameException
-	 *             The string passed as a parameter does not have the right
-	 *             format.
-	 * @throws NullPointerException
-	 *             if the name of the bean is not found into the environment
-	 *             (see {@link #GB_JMXBEAN_KEY})
-	 */
-	public static ServiceManager getProxy(Map<String, ?> environment,
-			JMXConnector jmxc) throws IOException,
-			MalformedObjectNameException, NullPointerException {
+        // init defaults
+        // url
+        if (!props.containsKey(GB_URL_KEY))
+            env.put(GB_URL_KEY, GB_URL);
+        else
+            env.put(GB_URL_KEY, props.getProperty(GB_URL_KEY));
 
-		if (jmxc == null || environment == null) {
-			throw new IllegalArgumentException("Some arguments are null");
-		}
+        // port
+        if (!props.containsKey(GB_PORT_KEY))
+            env.put(GB_PORT_KEY, GB_PORT);
+        else
+            env.put(GB_PORT_KEY, props.getProperty(GB_PORT_KEY));
 
-		// get bean name from the environment
-		final ObjectName mbeanName = new ObjectName("bean:name="
-				+ environment.get(GB_JMXBEAN_KEY));
+        if (!props.containsKey(GB_JMXBEAN_KEY))
+            env.put(GB_JMXBEAN_KEY, GB_JMXBEAN_NAME);
+        else
+            env.put(GB_JMXBEAN_KEY, props.getProperty(GB_JMXBEAN_KEY));
 
-		// create the proxy
-		final ServiceManager mbeanProxy = JMX.newMBeanProxy(
-				jmxc.getMBeanServerConnection(), mbeanName,
-				ServiceManager.class, true);
+        // form properties env to map env
+        final Set keySet = props.keySet();
+        final Iterator it = keySet.iterator();
+        while (it.hasNext()) {
+            String key = (String) it.next();
+            env.put(key, props.getProperty(key));
+        }
+        return env;
+    }
 
-		return mbeanProxy;
-	}
+    /**
+     * 
+     * @param env
+     * @return JMXConnector representing the newly-made connection. Each successful call to this method produces a different object
+     * @throws IllegalArgumentException if the passed env is null, or it doesn't contains valid values for the {@link #GB_PORT_KEY} of
+     *         {@link #GB_URL_KEY}
+     * @throws IOException if the connector client or the connection cannot be made because of a communication problem.
+     */
+    public static JMXConnector getConnector(Map<String, String> env)
+            throws IllegalArgumentException, IOException {
+        if (env == null)
+            throw new IllegalArgumentException("Unable to connect with a null environment, please");
+
+        final String gbUrl = env.get(GB_URL_KEY);
+        if (gbUrl == null)
+            throw new IllegalArgumentException(
+                    "Unable to connect with a null port, please set a port value for the key: "
+                            + GB_URL_KEY);
+
+        final String gbPort = env.get(GB_PORT_KEY);
+        if (gbPort == null)
+            throw new IllegalArgumentException(
+                    "Unable to connect with a null port, please set a url value for the key: "
+                            + GB_PORT_KEY);
+
+        StringBuilder urlString = new StringBuilder("service:jmx:rmi:///jndi/rmi://");
+
+        final JMXServiceURL url = new JMXServiceURL(urlString.append(gbUrl).append(":")
+                .append(gbPort).append("/jmxrmi").toString());
+
+        return JMXConnectorFactory.connect(url, env);
+    }
+
+    /**
+     * Using the initialized environment (see {@link #loadEnv(String)}) to create a dedicated proxy of the desired MBean (instead of going directly
+     * through the MBean server connection).
+     * 
+     * @param environment see {@link #loadEnv(String)}
+     * @return the ActionManager proxy instance.
+     * @throws IOException if a valid MBeanServerConnection cannot be created, for instance because the connection to the remote MBean server has not
+     *         yet been established (with the connect method), or it has been closed, or it has broken.
+     * @throws MalformedObjectNameException The string passed as a parameter does not have the right format.
+     * @throws NullPointerException if the name of the bean is not found into the environment (see {@link #GB_JMXBEAN_KEY})
+     */
+    public static ServiceManager getProxy(Map<String, ?> environment, JMXConnector jmxc)
+            throws IOException, MalformedObjectNameException, NullPointerException {
+
+        if (jmxc == null || environment == null) {
+            throw new IllegalArgumentException("Some arguments are null");
+        }
+
+        // get bean name from the environment
+        final ObjectName mbeanName = new ObjectName("bean:name=" + environment.get(GB_JMXBEAN_KEY));
+
+        // create the proxy
+        final ServiceManager mbeanProxy = JMX.newMBeanProxy(jmxc.getMBeanServerConnection(),
+                mbeanName, ServiceManager.class, true);
+
+        return mbeanProxy;
+    }
 
 }
