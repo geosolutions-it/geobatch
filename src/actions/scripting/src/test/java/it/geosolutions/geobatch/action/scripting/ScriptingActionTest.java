ba4c980f0808a694bbbea38eede38af9e96c71e7
Damianofds <damiano.giampaoli@geo-solutions.it>
2012-09-28 19:36:53 +0200

    #99 added license abd gt-TestData dependency

--- src/actions/scripting/src/test/java/it/geosolutions/geobatch/action/scripting/ScriptingActionTest.java
+++ src/actions/scripting/src/test/java/it/geosolutions/geobatch/action/scripting/ScriptingActionTest.java
@@ -1,4 +1,23 @@
 /*
+ *  GeoBatch - Open Source geospatial batch processing system
+ *  http://geobatch.geo-solutions.it/
+ *  Copyright (C) 2007-2012 GeoSolutions S.A.S.
+ *  http://www.geo-solutions.it
+ *
+ *  GPLv3 + Classpath exception
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package it.geosolutions.geobatch.action.scripting;
@@ -21,6 +40,7 @@ import java.util.Queue;
 
 import junit.framework.Assert;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
 
@@ -74,6 +94,14 @@ public class ScriptingActionTest extends Assert {
         assertEquals("13", outs.get(3));
         // unchanged local variable
         assertEquals(intVar, Integer.valueOf(12));
+    }
+    
+    @Ignore
+    @Test
+    public void testMassiveGroovyAction() throws ActionException, IOException{
+    	for(int i=0; i<10000; i++){
+    		testGroovyAction();
+    	}
     }
 
 }
