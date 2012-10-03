9327ba11ce68c06a1ca548e65898ee60fa4bf589
Damianofds <damiano.giampaoli@geo-solutions.it>
2012-10-03 18:59:47 +0200

    #97 added some tests, modified logging

--- src/users/src/main/java/it/geosolutions/geobatch/users/UsersDBPropertyOverrideConfigurer.java
+++ src/users/src/main/java/it/geosolutions/geobatch/users/UsersDBPropertyOverrideConfigurer.java
@@ -11,10 +11,9 @@ import it.geosolutions.tools.commons.file.Path;
 import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-import org.geotools.util.logging.Logging;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /**
  * 
  * Specific implementation of a {@link GeoBatchDataDirAwarePropertyOverrideConfigurer} for placing properly the database for
@@ -24,7 +23,7 @@ import org.geotools.util.logging.Logging;
  * <ol>
  *   <li> By default looks first for a property override file gb_database.properties inside the <b>settings</b> directory 
  *   <li> If there is no default gb_database.properties file inside the settings directory it tries to place the dbs inside the settings/database directory if that exists and is writable
- *   <li> It still allows users to specify locations from where loading the properties. This can be done by placing a gb_datastore.properties file inside the home directory of the GeoServer process or directly specificng a full path for property file via the DATABASE_CONFIG_FILE env property.
+ *   <li> It still allows users to specify locations from where loading the properties. This can be done by placing a gb_datastore.properties file inside the home directory of the user running geobatch or directly specifying a full path for property file via the DATABASE_CONFIG_FILE env property.
  * </ol>
  * @author Simone Giannecchini, GeoSolutions SAS.
  *
@@ -32,7 +31,7 @@ import org.geotools.util.logging.Logging;
 public class UsersDBPropertyOverrideConfigurer extends GeoBatchDataDirAwarePropertyOverrideConfigurer{
 	
 	/**Logger.*/
-	static final private Logger LOGGER= Logging.getLogger(UsersDBPropertyOverrideConfigurer.class);
+	private final static Logger LOGGER = LoggerFactory.getLogger(UsersDBPropertyOverrideConfigurer.class);
 	
 	/** 
 	 * Constructor..
@@ -49,8 +48,8 @@ public class UsersDBPropertyOverrideConfigurer extends GeoBatchDataDirAwarePrope
 		// do we have a gb_database.properties file inside the config dir?
 		final File file=Path.findLocation("settings/gb_database.properties", dataDirectoryHandler.getBaseConfigDirectory());
 		if(file==null||!(file.isFile()&&file.canRead()&&file.exists())){
-			if(LOGGER.isLoggable(Level.FINE)){
-				LOGGER.fine("Setting embedded database to work in the internal directory");
+			if(LOGGER.isInfoEnabled()){
+				LOGGER.info("Setting embedded database to work in the internal directory");
 			}
 			// move on with default
 			final File dbDir=Path.findLocation("settings/database", dataDirectoryHandler.getBaseConfigDirectory());
@@ -62,21 +61,21 @@ public class UsersDBPropertyOverrideConfigurer extends GeoBatchDataDirAwarePrope
 				return properties;
 			
 			} else {
-				if(LOGGER.isLoggable(Level.FINE)){
-					LOGGER.fine("Unable to set embedded database to work in the internal directory, check that such directory exists and it is writable");
+				if(LOGGER.isErrorEnabled()){
+					LOGGER.error("Unable to set embedded database to work in the internal directory, check that such directory exists and it is writable");
 				}	
 			}
 		} else {
 			if(file!=null&&file.isFile()&&file.canRead()&&file.exists()){
-				if(LOGGER.isLoggable(Level.FINE)){
-					LOGGER.fine("Loading default property file");
+				if(LOGGER.isInfoEnabled()){
+					LOGGER.info("Loading the database custom property file");
 				}
 
 				try {
 					return loadProperties(file);
 				} catch (IOException e) {
-					if(LOGGER.isLoggable(Level.SEVERE)){
-						LOGGER.log(Level.SEVERE,e.getLocalizedMessage(),e);
+					if(LOGGER.isErrorEnabled()){
+						LOGGER.error(e.getLocalizedMessage(),e);
 					}
 				}
 			} 			
---
+++ src/users/src/test/java/it/geosolutions/geobatch/dblocation/DBLocationTest.java
@@ -0,0 +1,95 @@
+package it.geosolutions.geobatch.dblocation;
+
+import static org.junit.Assert.assertEquals;
+import it.geosolutions.geobatch.catalog.file.DataDirHandler;
+import it.geosolutions.geobatch.users.UsersDBPropertyOverrideConfigurer;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Properties;
+
+import org.geotools.test.TestData;
+import org.junit.Test;
+
+/**
+ * 
+ * @author DamianoG
+ *
+ */
+public class DBLocationTest {
+
+	/**
+	 * Set a GEOBATCH_CONFIG_DIR mock location and test if the returned jdbc URLs correctly points 
+	 * to settings/database dir 
+	 * @throws Exception
+	 */
+	@Test
+	public void testNoGBDatabaseFile() throws Exception {
+		
+		File gbConfigDir = TestData.file(this, "config");
+		System.setProperty("GEOBATCH_CONFIG_DIR", gbConfigDir.getAbsolutePath());
+		DataDirHandler dataDirHandler = new DataDirHandler();
+		dataDirHandler.init();
+		UserConfigurerTester configurer = new UserConfigurerTester(dataDirHandler);
+		Properties prop = configurer.retrievePropertiesLoaded();
+		String ftp_server = (String)prop.get("dataSource-gb-ftp-server.jdbcUrl");
+		String users = (String)prop.get("dataSource-gb-users.jdbcUrl");
+		assertEquals(ftp_server, "jdbc:h2:" + gbConfigDir + "/settings/database/ftpusers");
+		assertEquals(users,  "jdbc:h2:" + gbConfigDir + "/settings/database/gbusers");
+	}
+	
+	/**
+	 * Set a GEOBATCH_CONFIG_DIR mock location, create the gb_database.properties file and test 
+	 * if the returned jdbc URLs correctly points to those specified in the file
+	 * @throws Exception
+	 */
+	@Test
+	public void testGBDatabaseFile() throws Exception {
+		
+		Properties gb_database = null;
+		File propGB = null;
+		
+		try{
+			File gbConfigDir = TestData.file(this, "config");
+			File gbSettingsDir = TestData.file(this, "config/settings");
+			
+			propGB = new File(gbSettingsDir + "/gb_database.properties");
+			gb_database = new Properties();
+			gb_database.setProperty("dataSource-gb-ftp-server.jdbcUrl", "jdbc:h2:OnlyForTestIfGBDATABASEisRead1");
+			gb_database.setProperty("dataSource-gb-users.jdbcUrl", "jdbc:h2:OnlyForTestIfGBDATABASEisRead2");
+			gb_database.store(new FileOutputStream(propGB), null);
+			
+    		System.setProperty("GEOBATCH_CONFIG_DIR", gbConfigDir.getAbsolutePath());
+			DataDirHandler dataDirHandler = new DataDirHandler();
+			dataDirHandler.init();
+			UserConfigurerTester configurer = new UserConfigurerTester(dataDirHandler);
+			Properties prop = configurer.retrievePropertiesLoaded();
+			String ftp_server = (String)prop.get("dataSource-gb-ftp-server.jdbcUrl");
+			String users = (String)prop.get("dataSource-gb-users.jdbcUrl");
+			assertEquals(ftp_server, "jdbc:h2:OnlyForTestIfGBDATABASEisRead1");
+			assertEquals(users,  "jdbc:h2:OnlyForTestIfGBDATABASEisRead2");
+		}
+		finally{
+			if (propGB != null && propGB.exists() && propGB.canWrite()){
+				propGB.delete();
+			}
+		}
+	}
+	
+	/**
+	 * Extends UsersDBPropertyOverrideConfigurer in order to expose the retrievePropertiesLoaded Method
+	 * @author damiano
+	 *
+	 */
+	private class UserConfigurerTester extends UsersDBPropertyOverrideConfigurer{
+		
+		public UserConfigurerTester(DataDirHandler dataDirHandler){
+			super(dataDirHandler);
+		}
+		
+		public Properties retrievePropertiesLoaded(){
+			return getCustomProperties();
+		}
+	}
+
+}
