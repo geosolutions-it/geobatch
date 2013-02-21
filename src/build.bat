438b26e1f35babcb462834d612048c32aa4ad96c
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2013-02-21 12:32:44 +0100

    Added ds2ds action for GeoTools DataStore to DataStore data copying and transforming

--- src/build.bat
+++ src/build.bat
@@ -4,7 +4,7 @@ rem profile needed for base demo (commons + geotiff + shapefiles)
 rem set PROFILES=geotiff,shapefile,commons
 
 rem profiles for base demo + task executor 
-set PROFILES=geotiff,shapefile,task-executor,freemarker,scripting,commons,imagemosaic
+set PROFILES=geotiff,shapefile,task-executor,freemarker,scripting,commons,imagemosaic,ds2ds
 
 rem other profiles unsupported yet
 rem PROFILES="$PROFILES,xstream,shp2pg,geonetwork,geostore,octave,jmx,jms"
