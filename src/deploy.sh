e97a73ffd27b76c17c10e257991e7119da1e3f69
carlo cancellieri <ccancellieri@hotmail.com>
2013-03-06 20:38:47 -0800

    fix root.basedir in pom.xml files

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,1 +1,4 @@
-MVN_OPTS="-Xmx1024m" mvn site install site:deploy -Pjetty -DskipTests=true -Dall
+# use this line to check for variable
+# for i in `find . -name pom.xml`; do echo $i;grep root.basedir $i; done
+
+MVN_OPTS="-Xmx1024m" mvn site install site:deploy -Pjetty -DskipTests=true -Dall $1 $2
