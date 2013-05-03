795d037c748c50f577e2ad2b732900a67f184a45
eblondel <emmanuel.blondel1@gmail.com>
2013-05-03 11:06:26 +0200

    fix Ds2dsGeoServerActionTest (remove use of GeoServerTests class)

--- src/actions/ds2ds/src/test/java/it/geosolutions/geobatch/actions/ds2ds/geoserver/Ds2dsGeoServerActionTest.java
+++ src/actions/ds2ds/src/test/java/it/geosolutions/geobatch/actions/ds2ds/geoserver/Ds2dsGeoServerActionTest.java
@@ -3,8 +3,6 @@ package it.geosolutions.geobatch.actions.ds2ds.geoserver;
 import it.geosolutions.filesystemmonitor.monitor.FileSystemEvent;
 import it.geosolutions.filesystemmonitor.monitor.FileSystemEventType;
 import it.geosolutions.geobatch.actions.ds2ds.dao.FeatureConfiguration;
-import it.geosolutions.geobatch.geoserver.test.GeoServerTests;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.net.URISyntaxException;
@@ -22,16 +20,34 @@ import com.thoughtworks.xstream.XStream;
  * The test is based on a postgis instance whose parameters
  * are specified in the input states.xml (see test-data/inputs/states.xml)
  * 
+ * Environment variable to set:
+ * <b>ds2ds_test</b> - string{true|false} - to enable|disable the tests
+ *
  * @author Emmanuel Blondel (FAO)
  *
  */
-public class Ds2dsGeoServerActionTest extends GeoServerTests{
+public class Ds2dsGeoServerActionTest{
 
+		private static boolean enabled = false;
 
 		static final Logger LOGGER = Logger.getLogger(Ds2dsGeoServerActionTest.class);
 		static final String FLOW_XML = "ds2dsGeoserver.xml";
 		
 		Ds2dsGeoServerConfiguration configuration;
+		
+		static{
+			enabled = getenv("ds2ds_test", "false").equalsIgnoreCase("true");
+	        if (!enabled) {
+	            LOGGER.warn("Tests on GeoServer are disabled. Please read the documentation to enable them.");
+	        } 
+		}
+		
+		public static String getenv(String envName, String envDefault) {
+	        String env = System.getenv(envName);
+	        String ret = System.getProperty(envName, env);
+	        LOGGER.debug("env var " + envName + " is " + ret);
+	        return ret != null ? ret : envDefault;
+	    }
 		
 		@Before
 		public void setUp() throws Exception {
@@ -40,26 +56,25 @@ public class Ds2dsGeoServerActionTest extends GeoServerTests{
 				Ds2dsGeoServerConfiguration.class);
 		configuration = (Ds2dsGeoServerConfiguration) stream.fromXML(this
 				.getResourceFile(FLOW_XML));
-
-		configuration.setGeoserverPWD(getPwd());
-		configuration.setGeoserverUID(getUid());
-		configuration.setGeoserverURL(getUrl());
 		
 		}
 		
 		@Test
 		public void testExecuteXml() throws Exception {
 
-			configuration.setFeatureConfiguration(FeatureConfiguration.fromXML(
-					new FileInputStream(getResourceFile("states.xml"))));
-			
-			Queue<EventObject> events = new LinkedList<EventObject>();
-			events.add(new FileSystemEvent(getResourceFile("states.xml"),
-					FileSystemEventType.FILE_ADDED));
+			if(enabled){
+				configuration.setFeatureConfiguration(FeatureConfiguration.fromXML(
+						new FileInputStream(getResourceFile("states.xml"))));
+				
+				Queue<EventObject> events = new LinkedList<EventObject>();
+				events.add(new FileSystemEvent(getResourceFile("states.xml"),
+						FileSystemEventType.FILE_ADDED));
 
-			Ds2dsGeoServerAction action = new Ds2dsGeoServerAction(configuration);
-			action.setTempDir(new File("/tmp"));
-			action.execute(events);
+				Ds2dsGeoServerAction action = new Ds2dsGeoServerAction(configuration);
+				action.setTempDir(new File("/tmp"));
+				action.execute(events);
+			}
+			
 		}
 		
 		private File getResourceFile(String resource) throws URISyntaxException {
